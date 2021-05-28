f5a93dbe17006e665189ba5a4d2db8a7c5a006b4
Dekguh <kadekteguhmahesa@gmail.com>
2021-05-28 15:31:35 +0800

    add data provinsi & kabupaten

--- comps/utils/json/data.js
+++ comps/utils/json/data.js
@@ -12,25 +12,49 @@ export const jsonListTypeJob = [{
     name: 'Part_Time'
 }]
 
-export const jsonListCategory = [{
-    id: 1,
-    name: 'Software_Engineering'
-},{
-    id: 2,
-    name: 'Finance'
-},{
-    id: 3,
-    name: 'Marketing'
-},{
-    id: 4,
-    name: 'Design'
-},{
-    id: 5,
-    name: 'Business_Development'
-},{
-    id: 6,
-    name: 'Driver'
-},{
-    id: 7,
-    name: 'Other'
-}]
+export const jsonListCategory = [
+    'Software_Engineering',
+    'Finance',
+    'Marketing',
+    'Design',
+    'Business_Development',
+    'Driver',
+    'Other'
+]
+
+export const jsonListProvinsi = [
+    'Aceh',
+    'Bali',
+    'Banten',
+    'Bengkulu',
+    'Yogyakarta',
+    'Jakarta',
+    'Gorontalo',
+    'Jambi',
+    'Jawa_Barat',
+    'Jawa_Tengah',
+    'Jawa_Timur',
+    'Kalimantan_Barat',
+    'Kalimantan_Selatan',
+    'Kalimantan_Tengah',
+    'Kalimantan_Timur',
+    'Kalimantan_Utara',
+    'Kepulauan_Bangka_Belitung',
+    'Kepulauan_Riau',
+    'Lampung',
+    'Maluku',
+    'Maluku_Utara',
+    'Nusa_Tenggara_Barat',
+    'Nusa_Tenggara_Timur',
+    'Papua',
+    'Papua_Barat',
+    'Riau',
+    'Sulawesi_Barat',
+    'Sulawesi_Selatan',
+    'Sulawesi_Tengah',
+    'Sulawesi_Tenggara',
+    'Sulawesi_Utara',
+    'Sumatra_Barat',
+    'Sumatra_Selatan',
+    'Sumatra_Utara'
+];
