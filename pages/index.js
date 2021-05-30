7e9664dc2f4579660776011598e50acccdf1134c
Dekguh <kadekteguhmahesa@gmail.com>
2021-05-30 09:33:40 +0800

    update section list latest job

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import HomeHero from '../comps/organisms/HomeHero';
+import LatestJobSection from '../comps/organisms/LatestJobSection';
 
 export default function Home() {
   return (
     <>
       <HomeHero />
+      <LatestJobSection />
     </>
   )
 }
