6c0318d952e0d6e28b1f117bc19d77de4f19f9a1
Dekguh <kadekteguhmahesa@gmail.com>
2021-05-30 12:27:13 +0800

    update LatestJobSection

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head';
 import HomeHero from '../comps/organisms/HomeHero';
 import LatestJobSection from '../comps/organisms/LatestJobSection';
+import { jsonListDataJob } from '../comps/utils/json/data';
 
 export default function Home() {
   return (
     <>
       <HomeHero />
-      <LatestJobSection />
+      <LatestJobSection classes='margin-top-50' dataLatest={jsonListDataJob} />
     </>
   )
 }
