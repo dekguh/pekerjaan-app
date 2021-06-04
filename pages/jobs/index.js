115368939d0bf157ab43ba9c531e65fd82187843
Dekguh <kadekteguhmahesa@gmail.com>
2021-06-04 14:44:30 +0800

    add userouter

--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -1,12 +1,15 @@
 import HeaderPage from "../../comps/organisms/HeaderPage";
 import JobArchive from "../../comps/organisms/JobArchive";
 import { jsonListDataJob } from '../../comps/utils/json/data';
+import { useRouter } from 'next/router';
 
 const JobArchivePage = ({ dataFresh }) => {
+    const Router = useRouter();
+
     return (
         <>
             <HeaderPage title='Lowongan Kerja' />
-            <JobArchive dataList={dataFresh} />
+            <JobArchive dataList={dataFresh} urlParams={Router.query} />
         </>
     )
 }
